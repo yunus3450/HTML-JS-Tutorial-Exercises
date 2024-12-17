4ecab4c86e891bbd0fc182c080db0521c27c8b3a
Yunus Polat <90975008+yunus3450@users.noreply.github.com>
2024-12-17 23:03:31 +0300

    SayacUygulamasi
    SayacUygulamasi

---
+++ SayacUygulamasi/assets/js/script.js
@@ -0,0 +1,11 @@
+let sayi = 0;
+
+    function sayiyiArtir() {
+      sayi++;
+      document.getElementById("sayi").textContent = sayi;
+    }
+
+    function sayiyiAzalt() {
+      sayi--;
+      document.getElementById("sayi").textContent = sayi;
+    }
