cd4ed75ec2694222e8bfc4ebe6b5e03c3a7529e2
Yunus Polat <90975008+yunus3450@users.noreply.github.com>
2024-12-17 23:00:26 +0300

    Metin-Gizle-Göster
    Metin-Gizle-Göster

---
+++ Metin-Gizle-Göster/assets/js/script.js
@@ -0,0 +1,8 @@
+function metniGizGoster() {
+      const metin = document.getElementById("metin");
+      if (metin.style.display === "none") {
+        metin.style.display = "block";
+      } else {
+        metin.style.display = "none";
+      }
+    }
