1d5015b14c576848e2fadc2c7dcf87df92ceefe9
Yunus Polat <90975008+yunus3450@users.noreply.github.com>
2024-12-18 15:01:01 +0300

    Metini Büyük Küçük Yapma
    Metini Büyük Küçük Yapma

---
+++ MetiniKucukBuyukYapma/assets/js/script.js
@@ -0,0 +1,8 @@
+function buyukYap() {
+      const metin = document.getElementById("metin").value;
+      document.getElementById("sonuc").innerText = metin.toUpperCase();
+    }
+    function kucukYap() {
+      const metin = document.getElementById("metin").value;
+      document.getElementById("sonuc").innerText = metin.toLowerCase();
+    }
